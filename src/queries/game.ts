79ae438c8b981f207ecbe0418fab899612393126
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-09-07 09:19:22 -0400

    Create fetch game query function

---
+++ src/queries/game.ts
@@ -0,0 +1,28 @@
+import { GenreType } from './genre';
+import { PlatformType } from './platform';
+import { PublisherType } from './publisher';
+
+export interface GameType {
+    id: number;
+    title: string;
+    images: string[];
+    description: string;
+    price: number;
+    createdAt: string;
+    updatedAt: string;
+    genres: GenreType[];
+    platforms: PlatformType[];
+    publishers: PublisherType[];
+}
+
+export async function getGames(): Promise<GameType[]> {
+    const response = await fetch(
+        `${process.env.API}/games?orderby=date&order=desc`
+    );
+
+    if (!response.ok) throw new Error("Server isn't responding!");
+
+    const data = await response.json();
+
+    return data.data.games;
+}
