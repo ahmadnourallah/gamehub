0bf26004ce6914f25ee65ed7923bfefaba1b2f59
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-11-02 11:21:48 -0500

    Add ability to fetch a genre's games

--- src/queries/genre.ts
+++ src/queries/genre.ts
@@ -1,5 +1,26 @@
+import type { GameType } from './game';
+
 export interface GenreType {
     id: number;
     name: string;
     createdAt: string;
+}
+
+export async function getGenreGames(
+    genreName: string,
+    start: number = 0,
+    end: number = 10,
+    search: string = '',
+    orderBy: 'date' | 'title' = 'date',
+    order: 'asc' | 'desc' = 'asc'
+): Promise<GameType[]> {
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API}/genres/${genreName}/games?start=${start}&end=${end}&search=${search}&orderby=${orderBy}&order=${order}`
+    );
+
+    if (!response.ok) throw new Error("Server isn't responding!");
+
+    const data = await response.json();
+
+    return data.data.games;
 }
