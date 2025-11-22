4e8ced9a9f973564eb974d54be5572d78456ab99
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-11-22 11:54:34 -0500

    Create types to handle 404 errors

--- src/queries/game.ts
+++ src/queries/game.ts
@@ -2,6 +2,24 @@ import { GenreType } from './genre';
 import { PlatformType } from './platform';
 import { PublisherType } from './publisher';
 
+type ResponseDataType<DataKey extends string, DataType> = {
+    [k in DataKey]: DataType;
+};
+
+interface FailResponseType {
+    status: 'fail';
+    code: number;
+}
+
+interface SuccessResponse<DataKey extends string, DataType> {
+    status: 'success';
+    data: ResponseDataType<DataKey, DataType>;
+}
+
+export type ResponseType<DataKey extends string, DataType> =
+    | SuccessResponse<DataKey, DataType>
+    | FailResponseType;
+
 export interface GameType {
     id: number;
     title: string;
@@ -33,12 +51,13 @@ export async function getGames(
     return data.data.games;
 }
 
-export async function getGame(id: number): Promise<GameType> {
+export async function getGame(
+    id: number
+): Promise<ResponseType<'game', GameType>> {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API}/games/${id}`);
 
-    if (!response.ok) throw new Error("Server isn't responding!");
+    if (!response.ok && response.status !== 404)
+        throw new Error("Server isn't responding!");
 
-    const data = await response.json();
-
-    return data.data.game;
+    return await response.json();
 }
