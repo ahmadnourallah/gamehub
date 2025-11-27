5c345a7dfe43018cc67a702de98b558a882df00a
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-11-27 12:08:52 -0500

    Make Slider more dynamic

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'motion/react';
-import { ReactNode, Reference, RefObject } from 'react';
+import { ReactNode, RefObject, useRef } from 'react';
 
 export default function Slider({
     isActive,
@@ -14,17 +14,23 @@ export default function Slider({
     className?: string;
     ref?: RefObject<HTMLDivElement | null>;
 }) {
+    const divRef = useRef<HTMLDivElement>(null);
+
     return (
         <motion.div
             ref={ref}
             transition={{ bounce: 0 }}
             animate={{
-                x: isActive ? 0 : position === 'left' ? -300 : 300,
+                x: isActive
+                    ? 0
+                    : position === 'left'
+                      ? -(divRef?.current?.offsetWidth || 300)
+                      : divRef?.current?.offsetWidth || 300,
                 visibility: isActive ? 'visible' : 'hidden'
             }}
             className={`absolute top-0 h-full ${position === 'left' ? 'left-0' : 'right-0'} z-80 ${className}`}
         >
-            {children}
+            <div ref={divRef}>{children}</div>
         </motion.div>
     );
 }
