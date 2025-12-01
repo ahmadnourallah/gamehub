1037dbe9342f61b7177e98fbdbd437e810cb98d0
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-12-01 12:25:00 -0500

    Add header to homepage

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -5,40 +5,44 @@ import Icon from '@mdi/react';
 import Button from '@/components/Button';
 import GameSlider from '@/components/GameSlider';
 import Hero from '@/components/Hero';
+import Header from '@/components/Header';
 
 export default async function Home() {
     const games = await getGames();
 
     return (
-        <div className="flex flex-col gap-10">
-            <Hero />
+        <>
+            <Header />
+            <div className="flex flex-col gap-10">
+                <Hero />
 
-            <div>
-                <div className="container mx-auto mb-4 flex flex-col items-center text-center">
-                    <h1 className="w-max border-b-2 border-b-white text-4xl font-bold">
-                        Latest Games
-                    </h1>
-                    <p className="m-4 text-lg">
-                        A select of the latest and best games you&apos;ll ever
-                        play
-                    </p>
+                <div>
+                    <div className="container mx-auto mb-4 flex flex-col items-center text-center">
+                        <h1 className="w-max border-b-2 border-b-white text-4xl font-bold">
+                            Latest Games
+                        </h1>
+                        <p className="m-4 text-lg">
+                            A select of the latest and best games you&apos;ll
+                            ever play
+                        </p>
+                    </div>
+
+                    <GameSlider games={games} />
+
+                    <div className="m-6 flex justify-center">
+                        <Link href="/store">
+                            <Button className="flex items-center gap-2">
+                                <Icon path={mdiGamepadVariant} size={1.2} />
+                                <span>Check out more</span>
+                            </Button>
+                        </Link>
+                    </div>
                 </div>
 
-                <GameSlider games={games} />
-
-                <div className="m-6 flex justify-center">
-                    <Link href="/store">
-                        <Button className="flex items-center gap-2">
-                            <Icon path={mdiGamepadVariant} size={1.2} />
-                            <span>Check out more</span>
-                        </Button>
-                    </Link>
-                </div>
+                <footer className="bg-[#202020] p-6 text-center text-lg font-bold">
+                    @ All rights reserved to Ahmad Nour Alla
+                </footer>
             </div>
-
-            <footer className="bg-[#202020] p-6 text-center text-lg font-bold">
-                @ All rights reserved to Ahmad Nour Alla
-            </footer>
-        </div>
+        </>
     );
 }
