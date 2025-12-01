85b3b68649e691d855a68a83ec059e4573735449
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-12-01 07:32:54 -0500

    Create a hook to handle ourside clicks

---
+++ src/utils/hooks.ts
@@ -0,0 +1,21 @@
+import { useRef, useEffect } from 'react';
+
+export const useOutsideClick = (callback: VoidFunction) => {
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClick = (event: MouseEvent) => {
+            if (ref.current && !ref.current.contains(event.target as Node)) {
+                callback();
+            }
+        };
+
+        document.addEventListener('click', handleClick, true);
+
+        return () => {
+            document.removeEventListener('click', handleClick, true);
+        };
+    }, [callback, ref]);
+
+    return ref;
+};
