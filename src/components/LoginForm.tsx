0af3f39b61351bde8b2ca8256e389b047bd4ff79
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-12-06 11:50:01 -0500

    Validate fields

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,51 +1,81 @@
 'use client';
 import { toast } from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { validateEmail, validatePassword } from '@/utils/validation';
 import Button from './Button';
 import loginAction from '@/actions/loginAction';
 import Link from 'next/link';
+import AuthInput from './AuthInput';
 
 export default function LoginForm() {
     const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [isEmailValid, setIsEmailValid] = useState(true);
+    const [password, setPassword] = useState('');
+    const [isPasswordValid, setIsPasswordValid] = useState(true);
 
     return (
         <form
-            className="flex flex-col gap-5 rounded-lg bg-[#202020] p-8 shadow-sm shadow-[rgba(0,255,255,0.7)]"
+            className="flex flex-col gap-2 rounded-lg bg-[#202020] p-8 shadow-sm shadow-[rgba(0,255,255,0.7)]"
             action={async (formData: FormData) => {
                 const res = await loginAction(formData);
 
                 if (res?.error) toast.error(res.error);
-                else router.push('/');
+                else {
+                    router.push('/');
+                    toast.success('Successfully logged in!');
+                }
             }}
         >
-            <label className="flex flex-col" htmlFor="email">
-                Email
-                <input
-                    id="email"
-                    className="w-60 rounded-md bg-white px-3 py-1 text-black sm:w-100"
-                    name="email"
-                    type="email"
-                    required
-                />
-            </label>
+            <AuthInput
+                label="Email"
+                name="email"
+                id="email"
+                value={email}
+                type="email"
+                isValid={isEmailValid}
+                onChange={(e) => {
+                    setEmail(e.target.value);
+                    if (validateEmail(e.target.value)) setIsEmailValid(true);
+                    else setIsEmailValid(false);
+                }}
+                validationMessage="* Email is not valid"
+            />
 
-            <label className="flex flex-col" htmlFor="password">
-                Password
-                <input
-                    id="password"
-                    className="w-60 rounded-md bg-white px-3 py-1 text-black sm:w-100"
-                    name="password"
-                    type="password"
-                    required
-                />
-            </label>
+            <AuthInput
+                label="Password"
+                name="password"
+                id="password"
+                value={password}
+                type="password"
+                isValid={isPasswordValid}
+                onChange={(e) => {
+                    setPassword(e.target.value);
+                    if (validatePassword(e.target.value))
+                        setIsPasswordValid(true);
+                    else setIsPasswordValid(false);
+                }}
+                validationMessage="* Minimum eight characters, at least one letter, one number
+                    and one special character"
+            />
+
             <p>
                 Don&apos;t have an account?{' '}
                 <Link className="text-[rgb(24,176,171)]" href="/signup">
                     Sign up
                 </Link>
             </p>
-            <Button>Login</Button>
+            <Button
+                disabled={
+                    !email.length ||
+                    !password.length ||
+                    !isEmailValid ||
+                    !isPasswordValid
+                }
+            >
+                Login
+            </Button>
         </form>
     );
 }
