45d686ff69752a9a5601b1446693fa0c165932fa
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-12-06 11:42:58 -0500

    Add styling for disabled state

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,16 +3,19 @@ import { MouseEventHandler, ReactNode } from 'react';
 export default function Button({
     children,
     className,
-    onClick
+    onClick,
+    disabled = false
 }: {
     children: ReactNode;
     className?: string;
     onClick?: MouseEventHandler;
+    disabled?: boolean;
 }) {
     return (
         <button
             onClick={onClick}
-            className={`scaleOnHover rounded-xl bg-white px-4 py-2 text-center text-lg text-black ![transition:background_200ms,scale,200ms] hover:bg-[rgb(106,190,187)] ${className}`}
+            className={`rounded-xl bg-white px-4 py-2 text-center text-lg text-black ![transition:background_200ms,scale,200ms] ${disabled ? 'bg-[rgb(106,190,187)]!' : 'scaleOnHover hover:bg-[rgb(106,190,187)]'} ${className}`}
+            disabled={disabled}
         >
             {children}
         </button>
