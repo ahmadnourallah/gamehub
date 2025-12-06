b2d473144acbe065d270eb3dee702a011de74a8d
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-12-06 12:33:16 -0500

    Center content

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,9 +9,9 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
     return (
-        <div className="mt-12 flex flex-col items-center justify-center">
+        <div className="flex h-screen flex-col items-center justify-center">
             <h1 className="text-center text-4xl">Not Found</h1>
-            <div className="mt-12 flex flex-col items-center rounded-lg bg-[#202020] p-8">
+            <div className="mt-12 flex flex-col items-center gap-4 rounded-lg bg-[#202020] p-8">
                 <p className="text-xl">Could not find requested resource</p>
                 <Link href="/" className="mt-8">
                     <Button>Return Home</Button>
