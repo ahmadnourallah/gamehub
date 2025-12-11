713349e88fc1ed0abff99a69f412754fa2057140
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-12-11 10:14:52 -0500

    Create game management page

---
+++ src/app/dashboard/games/page.tsx
@@ -0,0 +1,37 @@
+import { deleteGame, GameType, getGames } from '@/queries/game';
+import { MRT_ColumnDef } from 'mantine-react-table';
+import Button from '@/components/common/Button';
+import DashTable from '@/components/dashboard/DashTable';
+
+export default function DashGames() {
+    const columns: MRT_ColumnDef<GameType>[] = [
+        {
+            accessorKey: 'title',
+            header: 'Title'
+        },
+        {
+            accessorKey: 'price',
+            header: 'Price',
+            enableSorting: false
+        },
+        {
+            accessorKey: 'createdAt',
+            header: 'Created at'
+        }
+    ];
+
+    return (
+        <>
+            <div className="mb-4 flex justify-between">
+                <h1 className="text-4xl font-bold">Games</h1>
+                <Button>New</Button>
+            </div>
+            <DashTable
+                onDelete={deleteGame}
+                queryFn={getGames}
+                dataKey="games"
+                columns={columns}
+            />
+        </>
+    );
+}
