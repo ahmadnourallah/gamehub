086d8b667cdfa26c59572856ea061e30ae6546cc
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-12-11 10:13:18 -0500

    Create dashboard layout

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,44 @@
+import { ReactNode } from 'react';
+import { auth } from '@/auth';
+import { SessionProvider } from 'next-auth/react';
+import { mdiStore } from '@mdi/js';
+import { redirect } from 'next/navigation';
+import SignOutButton from '@/components/common/SignOutButton';
+import DashNavbar from '@/components/dashboard/DashNavbar';
+import VerticalDivider from '@/components/common/VerticalDivider';
+import IconButton from '@/components/common/IconButton';
+
+export default async function DashboardLayout({
+    children
+}: {
+    children: ReactNode;
+}) {
+    const session = await auth();
+
+    if (!session || !(session.user.role === 'ADMIN')) redirect('/');
+
+    return (
+        <SessionProvider>
+            <div className="mx-auto flex max-w-[1800px] gap-5">
+                <DashNavbar />
+                <main className="h-screen w-full overflow-auto p-6">
+                    <header className="mb-10 flex w-full justify-between">
+                        <div className="text-xl">
+                            Welcome, {session?.user.name}
+                        </div>
+                        <div className="flex">
+                            <SignOutButton />
+                            <VerticalDivider />
+                            <IconButton
+                                link="/store"
+                                tooltip="Store"
+                                iconPath={mdiStore}
+                            />
+                        </div>
+                    </header>
+                    {children}
+                </main>
+            </div>
+        </SessionProvider>
+    );
+}
---
+++ src/components/dashboard/DashNavbar.tsx
@@ -0,0 +1,91 @@
+'use client';
+import {
+    mdiCart,
+    mdiConsole,
+    mdiGamepad,
+    mdiGamepadVariant,
+    mdiHome,
+    mdiClose,
+    mdiMenu
+} from '@mdi/js';
+import { ReactNode, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import Slider from '../common/Slider';
+import Icon from '@mdi/react';
+import Link from 'next/link';
+import Logo from '@/components/common/Logo';
+
+function DashNavItem({
+    children,
+    icon,
+    link
+}: {
+    children: ReactNode;
+    icon: string;
+    link: string;
+}) {
+    const pathname = usePathname();
+
+    return (
+        <Link href={link} className="w-full">
+            <li
+                className={`flex items-center gap-2 px-4 py-6 transition-colors duration-300 hover:bg-white hover:text-black ${pathname === link ? 'bg-white text-black' : ''}`}
+            >
+                <Icon path={icon} size={1.2} />
+                <div className="text-lg">{children}</div>
+            </li>
+        </Link>
+    );
+}
+
+export default function DashNavbar() {
+    const [isActive, setIsActive] = useState(false);
+
+    return (
+        <>
+            <button
+                onClick={() => setIsActive(!isActive)}
+                className="fixed right-8 bottom-4 z-70 rounded-full bg-white p-1.5 text-2xl sm:hidden"
+            >
+                <Icon
+                    path={isActive ? mdiClose : mdiMenu}
+                    size={1.3}
+                    color="#000"
+                />
+            </button>
+            <Slider
+                className="bg-background h-full overflow-visible sm:!visible sm:static sm:!transform-none sm:p-0"
+                isActive={isActive}
+            >
+                <nav className="scrollbar-hidden h-screen overflow-y-auto bg-[rgb(32,32,32)]">
+                    <ul className="flex flex-col items-center text-xl">
+                        <li className="flex w-max items-center gap-4 p-4 select-none">
+                            <Logo />
+                        </li>
+                        <DashNavItem link="/dashboard" icon={mdiHome}>
+                            Home
+                        </DashNavItem>
+                        <DashNavItem link="/dashboard/carts" icon={mdiCart}>
+                            Carts
+                        </DashNavItem>
+                        <DashNavItem link="/dashboard/games" icon={mdiGamepad}>
+                            Games
+                        </DashNavItem>
+                        <DashNavItem
+                            link="/dashboard/genres"
+                            icon={mdiGamepadVariant}
+                        >
+                            Genres
+                        </DashNavItem>
+                        <DashNavItem
+                            link="/dashboard/platforms"
+                            icon={mdiConsole}
+                        >
+                            Platforms
+                        </DashNavItem>
+                    </ul>
+                </nav>
+            </Slider>
+        </>
+    );
+}
