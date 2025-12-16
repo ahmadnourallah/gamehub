5cc4134eb21d5f2cbdb4931ce42bcfcd30ce38b6
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-12-16 11:50:50 -0500

    Turn page into a client component to pass functions

--- src/app/dashboard/games/page.tsx
+++ src/app/dashboard/games/page.tsx
@@ -1,36 +1,40 @@
+'use client';
 import { deleteGame, getGames } from '@/actions/game';
 import type { GameType } from '@/lib/types';
 import { MRT_ColumnDef } from 'mantine-react-table';
 import Button from '@/components/common/Button';
 import DashTable from '@/components/dashboard/DashTable';
+import Link from 'next/link';
+
+const columns: MRT_ColumnDef<GameType>[] = [
+    {
+        accessorKey: 'title',
+        header: 'Title'
+    },
+    {
+        accessorKey: 'price',
+        header: 'Price',
+        enableSorting: false
+    },
+    {
+        accessorKey: 'createdAt',
+        header: 'Created at'
+    }
+];
 
 export default function DashGames() {
-    const columns: MRT_ColumnDef<GameType>[] = [
-        {
-            accessorKey: 'title',
-            header: 'Title'
-        },
-        {
-            accessorKey: 'price',
-            header: 'Price',
-            enableSorting: false
-        },
-        {
-            accessorKey: 'createdAt',
-            header: 'Created at'
-        }
-    ];
-
     return (
         <>
             <div className="mb-4 flex justify-between">
                 <h1 className="text-4xl font-bold">Games</h1>
-                <Button>New</Button>
+                <Link href="/dashboard/games/new">
+                    <Button>New</Button>
+                </Link>
             </div>
             <DashTable
+                dataKey="games"
                 onDelete={deleteGame}
                 queryFn={getGames}
-                dataKey="games"
                 columns={columns}
             />
         </>
