15c7e4858f4db4fc143fe581e8a195e3d13ed2ed
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-12-17 11:29:19 -0500

    Add functions for CRUD fetch requests

--- src/actions/platform.ts
+++ src/actions/platform.ts
@@ -1,10 +1,42 @@
-import type { QueryAllResponseType, GameType, PlatformType } from '@/lib/types';
+import type {
+    QueryAllResponseType,
+    QueryResponseType,
+    DeleteResponseType,
+    UpdateResponseType,
+    PlatformType,
+    GameType
+} from '@/lib/types';
 import { fetchAPI } from '@/lib/utils';
 
-export async function getPlatforms(): Promise<
-    QueryAllResponseType<'platforms', PlatformType[]>
-> {
-    return await fetchAPI('/platforms');
+export async function createPlatform(
+    formData: FormData,
+    token: string
+): Promise<UpdateResponseType<'platform', PlatformType>> {
+    return await fetchAPI(`/platforms`, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        body: formData
+    });
+}
+
+export async function getPlatform(
+    name: string
+): Promise<QueryResponseType<'platform', PlatformType>> {
+    return await fetchAPI(`/platforms/${name}`);
+}
+
+export async function getPlatforms(
+    start: number = 0,
+    end: number = 10,
+    search: string = '',
+    orderBy: 'date' | 'title' = 'date',
+    order: 'asc' | 'desc' = 'desc'
+): Promise<QueryAllResponseType<'platforms', PlatformType[]>> {
+    return await fetchAPI(
+        `/platforms?start=${start}&end=${end}&search=${search}&orderBy=${orderBy}&order=${order}`
+    );
 }
 
 export async function getPlatformGames(
@@ -18,4 +50,30 @@ export async function getPlatformGames(
     return await fetchAPI(
         `/platforms/${platformName}/games?start=${start}&end=${end}&search=${search}&orderBy=${orderBy}&order=${order}`
     );
+}
+
+export async function updatePlatform(
+    formData: FormData,
+    token: string,
+    name: string
+): Promise<UpdateResponseType<'platform', PlatformType>> {
+    return await fetchAPI(`/platforms/${name}`, {
+        method: 'PUT',
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        body: formData
+    });
+}
+
+export async function deletePlatform(
+    name: string,
+    token: string
+): Promise<DeleteResponseType> {
+    return await fetchAPI(`/platforms/${name}`, {
+        method: 'DELETE',
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
 }
