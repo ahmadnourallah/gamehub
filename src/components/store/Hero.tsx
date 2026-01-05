ad72b4de8e3309fa91dcfaddff43686f3e5cf4f5
Ahmad Nourallah <ahmadnurallah@gmail.com>
2026-01-05 11:24:58 -0500

    Add content to hero

--- src/components/store/Hero.tsx
+++ src/components/store/Hero.tsx
@@ -14,12 +14,12 @@ export default function Hero() {
             options={{ loop: true }}
             plugins={[AutoPlay()]}
         >
-            <div className="embla__container flex h-full">
+            <div className="embla__container flex h-full text-xl sm:text-2xl">
                 <div className="embla__slide relative h-full min-w-0 flex-[0_0_100%]">
                     <div className="absolute top-0 left-0 z-100 flex h-full w-full flex-col items-center justify-center gap-5 bg-[linear-gradient(rgba(0,0,0,0.2),rgba(0,0,0,0.2))] text-center">
                         <div>
                             <h1>GameHub</h1>
-                            <p className="m-2 text-xl">
+                            <p className="mt-4">
                                 All the games you&apos;re looking for in one
                                 place!
                             </p>
@@ -45,6 +45,28 @@ export default function Hero() {
                     />
                 </div>
                 <div className="embla__slide relative h-full min-w-0 flex-[0_0_100%]">
+                    <div className="absolute top-0 left-0 z-100 flex h-full w-full flex-col items-center justify-center gap-5 bg-[linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.2))] text-center">
+                        <div>
+                            <h1>Different Genres</h1>
+                            <div className="mt-4">
+                                <p>
+                                    It doesn&apos;t matter what your favorite
+                                    genere is.
+                                </p>
+                                <p>
+                                    We have something for all kinds of gamers!
+                                </p>
+                            </div>
+                        </div>
+
+                        <Link href="/store">
+                            <Button className="flex items-center gap-2">
+                                <Icon path={mdiGamepadVariant} size={1.2} />
+                                <span>Check them out</span>
+                            </Button>
+                        </Link>
+                    </div>
+
                     <Image
                         src="/hero2.jpg"
                         alt=""
@@ -58,6 +80,26 @@ export default function Hero() {
                     />
                 </div>
                 <div className="embla__slide relative h-full min-w-0 flex-[0_0_100%]">
+                    <div className="absolute top-0 left-0 z-100 flex h-full w-full flex-col items-center justify-center gap-5 bg-[linear-gradient(rgba(0,0,0,0.2),rgba(0,0,0,0.2))] text-center">
+                        <div>
+                            <h1>Every Platform</h1>
+                            <div className="mt-4">
+                                <p>All the platforms are supported.</p>
+                                <p>
+                                    Select a game for your favorite platform
+                                    now!
+                                </p>
+                            </div>
+                        </div>
+
+                        <Link href="/store">
+                            <Button className="flex items-center gap-2">
+                                <Icon path={mdiGamepadVariant} size={1.2} />
+                                <span>Check them out</span>
+                            </Button>
+                        </Link>
+                    </div>
+
                     <Image
                         src="/hero3.jpg"
                         alt=""
