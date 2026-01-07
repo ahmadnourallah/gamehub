99cff507b6af6cb6851b2f0d5cec3e6c82c44a8c
Ahmad Nourallah <ahmadnurallah@gmail.com>
2026-01-07 12:22:17 -0500

    Store image hostnames in environment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
+import { ExperimentalConfig } from 'next/dist/server/config-shared';
 
-const nextConfig: NextConfig = {
-    /* config options here */
+const nextConfig: NextConfig & ExperimentalConfig = {
+    images: {
+        remotePatterns: JSON.parse(process.env.HOSTNAMES || '[]').map(
+            (hostname: string) => ({
+                hostname
+            })
+        )
+    }
 };
 
 export default nextConfig;
